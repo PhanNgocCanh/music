22967d0ac0327d8c83ecd1fcf85512764fff20a4
PhanNgocCanh <canhrapter@gmail.com>
2023-04-24 07:54:44 +0700

    account-dto

---
+++ src/main/java/org/example/dto/AccountDTO.java
@@ -0,0 +1,43 @@
+package org.example.dto;
+
+public class AccountDTO {
+    public long accountId;
+
+    public String fullName;
+
+    public String password;
+
+    public int status;
+
+    public long getAccountId() {
+        return accountId;
+    }
+
+    public void setAccountId(long accountId) {
+        this.accountId = accountId;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public int getStatus() {
+        return status;
+    }
+
+    public void setStatus(int status) {
+        this.status = status;
+    }
+}
