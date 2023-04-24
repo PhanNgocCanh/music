6dbb4c0fe0fd2e3888f92708d1ffd671da493c92
PhanNgocCanh <canhrapter@gmail.com>
2023-04-24 07:57:32 +0700

    init-roleDTO

---
+++ src/main/java/org/example/dto/RoleDTO.java
@@ -0,0 +1,23 @@
+package org.example.dto;
+
+public class RoleDTO {
+    public long roleId;
+
+    public String roleName;
+
+    public long getRoleId() {
+        return roleId;
+    }
+
+    public void setRoleId(long roleId) {
+        this.roleId = roleId;
+    }
+
+    public String getRoleName() {
+        return roleName;
+    }
+
+    public void setRoleName(String roleName) {
+        this.roleName = roleName;
+    }
+}
